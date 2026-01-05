9ac3fc0b127ef8bf3ad3bac13641bcfa569b2a8d
codepackcommunity <codepackcommunity@gmail.com>
2026-01-05 04:52:16 +0200

    Xcode 10xffx operatins x10fxm5041

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,11 @@ export const metadata = {
   icon: "favicon.ico",
   title: "KM Electronix - Your Ultimate Phone Plug",
   description: "Your Ultimate Phone Plug | Developed By Cod3pack",
-
+   keywords: ['km', 'about', 'company', 'team', 'electronics', 'electronix', 'phones', 'price'],
+  openGraph: {
+    title: "KM Electronix - Your Ultimate Phone Plug",
+    description: "Stay Connected, Stay Ahead With KM Electronix."
+  },
 };
 
 export default function RootLayout({ children }) {
--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   devIndicators: false,
-  pageExtensions: ['(?<!\\.test\\.)ts', '(?<!\\.test\\.)tsx', '(?<!\\.test\\.)js', '(?<!\\.test\\.)jsx'],
 
 };
 
